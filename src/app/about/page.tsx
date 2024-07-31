6c1efce53b619a5f63236ffb5bc78ebb94696555
yash rai <yash2154rai@gmail.com>
2024-07-31 14:56:52 +0530

    create education and coursework in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,60 +3,98 @@ import aboutpageimg from "@/app/assets/aboutpage.png";
 import aboutpagesecond from "@/app/assets/aboutsecondsection.png";
 import Link from "next/link";
 import { Instagram, Linkedin, Twitter, Mail } from "lucide-react";
+import education from "@/app/assets/education.png";
 
 export default function about() {
   return (
-    <div className=" bg-gray-950 md:h-[160vh] h-[220vh] flex flex-col items-start justify-center  ">
-      <section className=" h-[80vh]  flex items-center justify-center gap-5  ">
-        <div className=" flex flex-col md:flex-row items-center justify-center">
-          <div className="w-[50%] flex flex-col gap-5">
-            <h1 className=" text-4xl font-bold text-center text-white">
-              Introduction
-            </h1>
-            <p className=" text-left text-lg text-white">
-              Hi, I&apos;m yash rai, a passionate software engineer dedicated to
-              crafting innovation solutions that drive positive change. With a
-              keen eye for detail and a passion for problem-solving, I strive to
-              push the boundaries of what&apos;s possible through technology.
-            </p>
+    <>
+      <div className=" bg-gray-950 md:h-[200vh] h-[350vh] flex flex-col items-start justify-center ">
+        <section className=" h-[80vh]  flex items-center justify-center gap-5 relative top-[-20rem] md:static ">
+          <div className=" flex flex-col md:flex-row items-center justify-center">
+            <div className="w-[50%] flex flex-col gap-5">
+              <h1 className=" text-4xl font-bold text-center text-white">
+                Introduction
+              </h1>
+              <p className=" text-left text-lg text-white">
+                Hi, I&apos;m yash rai, a passionate software engineer dedicated
+                to crafting innovation solutions that drive positive change.
+                With a keen eye for detail and a passion for problem-solving, I
+                strive to push the boundaries of what&apos;s possible through
+                technology.
+              </p>
+            </div>
+            <div>
+              <Image
+                src={aboutpageimg}
+                height={500}
+                width={500}
+                alt="image"
+                className=" h-[300px] w-[300px] md:h-[500px] md:w-[500px]"
+              ></Image>
+            </div>
           </div>
-          <div>
-            <Image
-              src={aboutpageimg}
-              height={500}
-              width={500}
-              alt="image"
-              className=" h-[300px] w-[300px] md:h-[500px] md:w-[500px]"
-            ></Image>
+        </section>
+        <section className=" h-[80vh] w-full flex items-center justify-center mx-auto mt-36 md:mt-4 relative top-[-20rem] md:static ">
+          <div className=" flex flex-col-reverse md:flex-row items-center justify-center gap-5 ">
+            <div>
+              <Image
+                src={aboutpagesecond}
+                height={400}
+                width={400}
+                alt="image"
+                className=" rounded-full  h-[300px] w-[300px] md:h-[400px] md:w-[400px]"
+              ></Image>
+            </div>
+            <div className="w-[50%] flex flex-col gap-5">
+              <h1 className=" text-4xl font-bold text-center text-white">
+                My Vision
+              </h1>
+              <p className=" text-left text-lg text-white">
+                My vision is to leverage my skills in software engineering to
+                create impactfull projects that makes a difference in
+                people&apos;s lives. I strive to develop intuitive applications,
+                stay ahead of technological advancements, collaborate with
+                like-minded individuals and continuously learn to ensure my
+                skills remain sharp.
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className=" h-[80vh] w-full flex items-center justify-center mx-auto mt-36 md:mt-4">
-        <div className=" flex flex-col-reverse md:flex-row items-center justify-center gap-5 ">
-          <div className="">
-            <Image
-              src={aboutpagesecond}
-              height={400}
-              width={400}
-              alt="image"
-              className=" rounded-full  h-[300px] w-[300px] md:h-[400px] md:w-[400px]"
-            ></Image>
+        </section>
+
+        <section className=" h-[80vh] w-full flex items-center justify-center mx-auto mt-36 md:mt-4">
+          <div className=" flex flex-col md:flex-row items-center justify-center gap-5 ">
+            <div className="w-[50%] flex flex-col gap-5">
+              <h1 className=" text-4xl font-bold text-center text-white">
+                Education and Coursework
+              </h1>
+              <p className=" text-left text-lg text-white">
+                I hold a degree in Computer Science, where I honed my technical
+                skills and developed a solid foundation in software engineering
+                principles. Throughout my academic journey, I engaged in
+                rigorous coursework that covered a broad spectrum of topics,
+                including data structures and algorithms, software design and
+                architecture, web development, and database management. I also
+                participated in various projects and internships that allowed me
+                to apply theoretical knowledge to practical scenarios, enhancing
+                my problem-solving abilities and teamwork skills. My education
+                has equipped me with the tools and knowledge necessary to tackle
+                complex challenges and contribute effectively to the tech
+                industry.
+              </p>
+            </div>
+            <div>
+              <Image
+                src={education}
+                height={400}
+                width={400}
+                alt="image"
+                className=" rounded-full  h-[300px] w-[300px] md:h-[400px] md:w-[400px] bg-none"
+              ></Image>
+            </div>
           </div>
-          <div className="w-[50%] flex flex-col gap-5">
-            <h1 className=" text-4xl font-bold text-center text-white">
-              My Vision
-            </h1>
-            <p className=" text-left text-lg text-white">
-              My vision is to leverage my skills in software engineering to
-              create impactfull projects that makes a difference in
-              people&apos;s lives. I strive to develop intuitive applications,
-              stay ahead of technological advancements, collaborate with
-              like-minded individuals and continuously learn to ensure my skills
-              remain sharp.
-            </p>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
+
       <div className="relative w-full h-[15vh] bg-gray-950 bottom-0 flex flex-row items-center justify-evenly ">
         <div className=" flex flex-row gap-10">
           <div className="  hover:underline hover:text-blue-500 text-white">
@@ -97,6 +135,6 @@ export default function about() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
