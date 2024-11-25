43444bab11424a3410749f352696d37e07613774
Yash rai <yash2154rai@gmail.com>
2024-11-25 22:53:08 +0530

    fix css

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -10,27 +10,27 @@ export default function Skills() {
       <div className="flex flex-col md:flex-row  justify-center md:gap-5 md:p-5 gap-20 mt-10 items-center ">
         <div className="card bg-base-100 w-80 md:w-96 h-[30rem] md:h-[28rem] shadow-2xl">
           <figure>
-            <Image src={frontendimg} alt="frontend" />
+            <Image src={frontendimg} alt="frontend" height={300} width={400}/>
           </figure>
-          <div className="card-body bg-neutral-900 text-white">
+          <div className="card-body bg-neutral-950 opacity-95 text-white">
             <h2 className="card-title">Frontend Development</h2>
             <p>HTML, CSS, JavaScript, ReactJS, NextJS, Redux, TypeScipt</p>
           </div>
         </div>
         <div className="card bg-base-100 w-80 md:w-96  h-[30rem]  md:h-[28rem] shadow-2xl">
           <figure>
-            <Image src={backendimg} alt="backend" />
+            <Image src={backendimg} alt="backend"  height={300} width={400}/>
           </figure>
-          <div className="card-body bg-neutral-900 text-white">
+          <div className="card-body bg-neutral-950 opacity-95 text-white">
             <h2 className="card-title">Backend Development</h2>
             <p>Python, NodeJS, FastAPI, ExpressJS, C++</p>
           </div>
         </div>
         <div className="card bg-base-100 w-80 md:w-96  h-[30rem]  md:h-[28rem] shadow-2xl">
           <figure>
-            <Image src={databaseimg} alt="database" />
+            <Image src={databaseimg} alt="database" height={300} width={400} />
           </figure>
-          <div className="card-body bg-neutral-900 text-white ">
+          <div className="card-body bg-neutral-950 opacity-95 text-white ">
             <h2 className="card-title">Database and Version control</h2>
             <p>Git, Github, MongoDB, MySQL, Docker</p>
           </div>
