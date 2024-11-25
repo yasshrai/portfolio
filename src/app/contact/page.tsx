55703205151e1b3738d1e49a5f9a717457b55d75
Yash rai <yash2154rai@gmail.com>
2024-11-25 21:47:53 +0530

    fix page.tsx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function page(){
+    return <div className="flex items-center justify-center font-extrabold h-40">coming soon </div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,9 +77,9 @@ export default function Home() {
         <div className="max-w-4xl w-screen mx-auto text-center">
           <h2 className={`text-3xl md:text-4xl font-bold mb-6 ${kanit.className}`}>Welcome to My Digital Space</h2>
           <p className="text-lg mb-8 text-zinc-400">
-            I'm passionate about creating innovative solutions and pushing the boundaries of technology. 
+            I&apos;mm passionate about creating innovative solutions and pushing the boundaries of technology. 
             With a strong foundation in full-stack development and a never-ending curiosity, 
-            I'm always excited to take on new challenges and learn cutting-edge technologies.
+            I&apos;m always excited to take on new challenges and learn cutting-edge technologies.
           </p>
           <Link href="/about" className="inline-flex items-center px-6 py-3 bg-zinc-900 rounded-full hover:bg-zinc-800 transition-colors">
             Learn More About Me <ArrowRight className="ml-2" />
@@ -129,10 +129,10 @@ export default function Home() {
         className="py-16 px-4 md:px-8 bg-zinc-900"
       >
         <div className="max-w-4xl w-screen mx-auto text-center">
-          <h2 className={`text-3xl md:text-4xl font-bold mb-6 ${kanit.className}`}>Let's Create Something Amazing</h2>
+          <h2 className={`text-3xl md:text-4xl font-bold mb-6 ${kanit.className}`}>Let&apos;s Create Something Amazing</h2>
           <p className="text-lg mb-8 text-zinc-400">
-            I'm always open to new opportunities, collaborations, and exciting projects. 
-            Whether you have a specific idea in mind or just want to connect, I'd love to hear from you!
+            I&apos;m always open to new opportunities, collaborations, and exciting projects. 
+            Whether you have a specific idea in mind or just want to connect, I&apos;d love to hear from you!
           </p>
           <Link href="/contact" className="inline-flex items-center px-8 py-4 bg-zinc-950 text-white rounded-full hover:bg-zinc-900 transition-colors font-bold">
             Get In Touch <ArrowRight className="ml-2" />
