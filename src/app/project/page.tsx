087752259351e874ae71599542bbf5d4cfd130ba
Yash rai <yash2154rai@gmail.com>
2024-11-27 13:24:02 +0530

    add new project card

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,7 +1,9 @@
 import ProjectCard from "./ProjectCard";
 import chatappimage from "@/app/assets/chatappbg.jpg";
 import bookstorebg from "@/app/assets/bookstorebg.png";
+import portfoliobg from "@/app/assets/portfolioproject.jpg"
 import adminpanelbg from "@/app/assets/adminpanel.jpg";
+
 
 const projects = [
   {
@@ -25,13 +27,20 @@ const projects = [
       "A college admin panel built with Next.js and Express.js provides a dynamic and efficient interface for managing student records, courses, and faculty data, featuring secure authentication and real-time updates.",
     link: "https://collegeadminpanel.vercel.app",
   },
+{
+  image: portfoliobg,
+  title: "Portfolio",
+  description:
+    "A personal portfolio website showcasing my skills, projects, and professional journey. Built with modern web technologies, it features an elegant design, responsive layout, and smooth navigation to provide visitors with a seamless experience while exploring my work and achievements.",
+  link: "https://yashrai.vercel.app",
+}
 
 ];
 
 export default function Project() {
   return (
-    <div className="bg-zinc-950 h-[240vh] md:h-[90vh] w-full ">
-      <div className="flex flex-col md:flex-row justify-center md:gap-5 md:p-5 gap-20 mt-10 items-center ">
+    <div className="bg-zinc-950 h-auto md:h-[90vh] w-full">
+      <div className="flex flex-wrap justify-center md:gap-5 gap-10 p-5 mt-10 items-center">
         {projects.map((project, index) => (
           <ProjectCard
             key={index}
