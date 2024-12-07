e239b469ffd4c76648766b6b708020f3d9417ab9
Yash rai <yash2154rai@gmail.com>
2024-12-07 18:49:58 +0530

    shadow effect in skills card

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -8,7 +8,7 @@ export default function Skills() {
   return (
     <div className="bg-zinc-950 h-[240vh] md:h-[90vh] w-full ">
       <div className="flex flex-col md:flex-row  justify-center md:gap-5 md:p-5 gap-20 mt-10 items-center ">
-        <div className="card bg-base-100 w-80 md:w-96 h-[30rem] md:h-[28rem] shadow-2xl">
+        <div className="card bg-base-100 w-80 md:w-96 h-[30rem] md:h-[28rem] shadow-2xl hover:outline outline-2  outline-blue-950 hover:shadow-2xl hover:shadow-blue-950">
           <figure>
             <Image src={frontendimg} alt="frontend" height={300} width={400}/>
           </figure>
@@ -17,7 +17,7 @@ export default function Skills() {
             <p>HTML, CSS, JavaScript, ReactJS, NextJS, Redux, TypeScipt</p>
           </div>
         </div>
-        <div className="card bg-base-100 w-80 md:w-96  h-[30rem]  md:h-[28rem] shadow-2xl">
+        <div className="card bg-base-100 w-80 md:w-96  h-[30rem]  md:h-[28rem] shadow-2xl hover:outline outline-2  outline-blue-950 hover:shadow-2xl hover:shadow-blue-950">
           <figure>
             <Image src={backendimg} alt="backend"  height={300} width={400}/>
           </figure>
@@ -26,7 +26,7 @@ export default function Skills() {
             <p>Python, NodeJS, FastAPI, ExpressJS, C++</p>
           </div>
         </div>
-        <div className="card bg-base-100 w-80 md:w-96  h-[30rem]  md:h-[28rem] shadow-2xl">
+        <div className="card bg-base-100 w-80 md:w-96  h-[30rem]  md:h-[28rem] shadow-2xl hover:outline outline-2  outline-blue-950 hover:shadow-2xl hover:shadow-blue-950">
           <figure>
             <Image src={databaseimg} alt="database" height={300} width={400} />
           </figure>
